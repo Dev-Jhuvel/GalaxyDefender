11aabc5ec7e02c90c60db780beacbab37b902fbe
Dev-Jhuvel <147971284+Dev-Jhuvel@users.noreply.github.com>
2024-01-26 14:10:19 +0800

    Add files via upload
    galaxy Defender need edit

---
+++ GalaxyDefender/src/galaxydefender/Commons.java
@@ -0,0 +1,26 @@
+
+package galaxydefender;
+
+public interface Commons {
+
+    int BOARD_WIDTH = 500;
+    int BOARD_HEIGHT = 500;
+    int BORDER_RIGHT = 30;
+    int BORDER_LEFT = 5;
+
+    int GROUND = 400;
+    int BOMB_HEIGHT = 5;
+
+    int ALIEN_HEIGHT = 16;
+    int ALIEN_WIDTH = 30;
+    int ALIEN_INIT_X = 150;
+    int ALIEN_INIT_Y = 20;
+
+    int GO_DOWN = 10;
+    int NUMBER_OF_ALIENS_TO_DESTROY = 5;
+    int CHANCE = 100;
+    int DELAY = 17;
+    int PLAYER_WIDTH = 5;
+    int PLAYER_HEIGHT = 5;
+}
+
---
+++ GalaxyDefender/src/galaxydefender/GalaxyDefender.java
@@ -0,0 +1,65 @@
+
+package galaxydefender;
+//import java.awt.Container;
+import java.awt.EventQueue;
+//import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class GalaxyDefender extends JFrame {
+
+    //JButton strtbtn, hghscrbtn, quitbtn;
+    //JFrame frame = new JFrame();
+    
+    public GalaxyDefender() {
+    /*
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBounds(0, 0, 500, 500);
+    Container container = frame.getContentPane();
+    container.setLayout(null);
+    frame.setVisible(true);
+            
+        strtbtn = new JButton(" Start Game");
+        strtbtn.setBounds(150, 10, 200, 40);
+        container.add(strtbtn);
+        strtbtn.addActionListener(e -> frame.setVisible(false));
+        
+        
+        hghscrbtn = new JButton("High Score");
+        hghscrbtn.setBounds(150, 70, 200, 40);
+        container.add(hghscrbtn);
+
+        
+        quitbtn = new JButton("Quit Game");
+        quitbtn.setBounds(150, 130, 200, 40);
+        container.add(quitbtn);
+       
+       */ 
+         initUI();
+    }
+
+    public void initUI() {
+
+        
+        
+        add(new Board());
+
+        setTitle("Galaxy Defender Game");
+        setSize(Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = new GalaxyDefender();
+            ex.setVisible(true);
+        });
+    }
+
+    
+}
